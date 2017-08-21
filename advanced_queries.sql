b2540072d6d568bcb99e26667ed8fc241692c433
m2creger <marshall.creger@gmail.com>
2017-08-21 08:52:52 -0600

    homework yo

--- advanced_queries.sql
+++ advanced_queries.sql
@@ -2,17 +2,20 @@
 -- For all of these queries, you SHOULD NOT use an id # in a WHERE clause
 
 -- Find all fields (book and author related) for all books written by George R.R. Martin.
-
+SELECT books.title FROM books LEFT OUTER JOIN authors ON authors.id = books.author_id WHERE authors.name = 'George R.R. Martin';
 -- Find all fields (book and author related) for all books written by Milan Kundera.
+SELECT books.title FROM books LEFT OUTER JOIN authors ON authors.id = books.author_id WHERE authors.name = 'Milan Kundera';
 
 -- Find all books written by an author from China or the UK.
-
+ SELECT books.title FROM books LEFT OUTER JOIN authors ON authors.id = books.author_id WHERE authors.nationality = 'China' OR authors.nationality = 'United States of America';
 -- Find out how many books Albert Camus wrote.
-
+SELECT books.title FROM books LEFT OUTER JOIN authors ON authors.id = books.author_id WHERE authors.name = 'Albert Camus';
 -- Find out how many books written before 1980 were by authors not from the US.
+SELECT books.title FROM books LEFT OUTER JOIN authors ON authors.id = books.author_id WHERE books.publication_date < 1980 AND authors.nationality != 'United States of America';
 
 -- For these last two, you should not need a JOIN.
 
 -- Find all authors whose names start with 'J'.
-
+SELECT name FROM authors WHERE name LIKE 'J%';
 -- Find all books whose titles contain 'the'.
+SELECT title FROM books WHERE title LIKE '%the%';
--- basic_queries.sql
+++ basic_queries.sql
@@ -2,30 +2,33 @@
 -- Unless otherwise stated, all queries should return all columns
 
 -- Get all information about all authors
-
+SELECT * FROM authors;
 -- Get just the name and birth year of all authors
-
+SELECT name, birth_year FROM authors;
 -- Get all authors born in the 20th centruy or later
-
+SELECT name  FROM authors WHERE birth_year > 1900;
 -- Get all authors born in the USA
-
+SELECT name FROM authors WHERE nationality = 'United States of America';
 -- Get all books published on 1985
-
+SELECT name FROM authors WHERE birth_year = '1985';
 -- Get all books published before 1989
-
+SELECT title FROM books WHERE publication_date < 1989;
 -- Get just the title of all books.
-
+SELECT title FROM books;
 -- Get just the year that 'A Dance with Dragons' was published
   -- Cry when you realize how long it's been
-
+SELECT publication_date FROM books where title = 'A Dance with Dragons';
 -- Get all books which have `the` somewhere in their title (hint, look up LIKE/ILIKE)
-
+SELECT * FROM books WHERE title LIKE '%the%';
 -- Add yourself as an author
-
+ INSERT INTO authors VALUES(9, 'Marshall Creger', 'United States of America', '1980');
 -- Add two books that you'd like to write (you can hard-code your id as the author id)
-
+INSERT INTO books VALUES(69, 'Why dogs are so awesome', '2017', 9);
+INSERT INTO books VALUES(70, 'What is next for America', 2017, 9);
 -- Update one of your books to have a new title
+UPDATE books SET title = 'Why dogs are so incredibly awesome' WHERE title = 'Why dogs are so awesome';
 
 -- Delete your books
-
+DELETE FROM books where title = 'Why dogs are so incredibly awesome';
 -- Delete your author entry
+DELETE FROM authors where name = 'Marshall Creger';
